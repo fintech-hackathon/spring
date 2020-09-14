83a86897103151c237a42f239fa25bcda27a3cc4
이원호 <dldnjsgh71@naver.com>
2020-09-14 20:38:17 +0900

    MASTER - add all firle

---
+++ singsong/src/main/java/com/singsong/singsong/service/user/userService.java
@@ -0,0 +1,9 @@
+package com.singsong.singsong.service.user;
+
+import com.singsong.singsong.dto.user.User;
+
+public interface userService {
+    User getUser(String id);
+    int joinUser(User user);
+    User login(User user);
+}
